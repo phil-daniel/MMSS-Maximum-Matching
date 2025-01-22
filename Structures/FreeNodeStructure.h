cfb9198b4e38f762d1186b1e9930ef2e2f51bf7c
Phillip Daniel <fv22283@bristol.ac.uk>
2025-01-22 21:12:33 +0000

    Continued work on algorithm
    Focusing on the cases within extend_active_path

--- Structures/FreeNodeStructure.h
+++ Structures/FreeNodeStructure.h
@@ -5,6 +5,7 @@ class FreeNodeStructure {
     public:
         bool on_hold = false;
         bool modified = false;
+        int vertices_count = 0;
 };
 
 #endif //FREENODESTRUCTURE_H
--- maxMatching.cpp
+++ maxMatching.cpp
@@ -2,23 +2,81 @@
 #include <iostream>
 #include <set>
 
+// Required for hashing pairs
+#include <boost/container_hash/hash.hpp>
+
 #include "Stream/Stream.h"
 #include "Stream/StreamFromFile.h"
 #include "Stream/StreamFromMemory.h"
 
+#include "Structures/FreeNodeStructure.h"
+
 using namespace std;
 
-typedef pair<int,int> Edge;
+typedef int Vertex;
+typedef pair<Vertex, Vertex> Edge;
 typedef set<Edge> Matching;
-typedef unordered_map<Edge,int> MatchingToLabel;
+typedef unordered_map<Edge, int, boost::hash<Edge>> MatchingToLabel;
 
-vector<vector<Edge>> alg_phase(Stream* stream, Matching matching, float epsilon, float scale) {
+void extend_active_path(
+    Stream* stream,
+    Matching matching,
+    float epsilon,
+    vector<FreeNodeStructure> free_node_structs,
+    vector<vector<Edge>> disjoint_augmenting_paths,
+    set<Vertex> removed_vertices
+) {
+
+    unordered_map<Vertex, FreeNodeStructure*> vertex_to_free_node_struct;
+
+    Edge edge = stream->readStream();
+    // edges are only -1 if we have reached the end of the stream.
+    while (edge.first != -1) {
+        // If we have "removed" one of the vertices from the graph, we skip this edge.
+        if (
+            removed_vertices.find(edge.first) != removed_vertices.end()
+            || removed_vertices.find(edge.second) != removed_vertices.end()
+        ) {
+            continue;
+        }
+
+        // TODO: Case 2 - to do with blossoms
+
+        // If the first vertex is in a "marked" or "on hold" structure, we skip this edge.
+        if (
+            vertex_to_free_node_struct.count(edge.first)
+            && (
+                vertex_to_free_node_struct[edge.first]->modified
+                || vertex_to_free_node_struct[edge.first]->on_hold
+            )
+        ) {
+            continue;
+        }
+
+
+        // Reading next edge
+        edge = stream->readStream();
+    }
+
+}
+
+
+vector<vector<Edge>> alg_phase(
+    Stream* stream,
+    Matching matching,
+    float epsilon,
+    float scale
+) {
     vector<vector<Edge>> disjoint_augmenting_paths = {};
+
+    set<Vertex> removed_vertices = {};
 
     MatchingToLabel matching_to_label;
 
     int path_limit = static_cast<int>(6 / scale) + 1;
     int pass_bundles_max = static_cast<int>(72 / (scale * epsilon));
+
+    vector<FreeNodeStructure> free_node_structs;
 
     // Setting the current distance for each matched edge to infinity.
     for (Edge edge : matching) {
@@ -26,14 +84,24 @@ vector<vector<Edge>> alg_phase(Stream* stream, Matching matching, float epsilon,
     }
 
     for (int pass_bundle = 0; pass_bundle < pass_bundles_max; pass_bundle++) {
+        // TODO: CHECK EACH OF THE FREE VERTEX STRUCTURES HERE
+        for (FreeNodeStructure free_node_struct : free_node_structs) {
+            if (free_node_struct.vertices_count >= path_limit) free_node_struct.on_hold = true;
+            else free_node_struct.on_hold = false;
+            free_node_struct.modified = false;
+        }
 
+        // TODO: IMPLEMENT ALGORITHM HERE!
     }
 
 
     return disjoint_augmenting_paths;
 }
 
-Matching augment_matching(Matching matching, vector<vector<Edge>> disjoint_augmenting_paths) {
+Matching augment_matching(
+    Matching matching,
+    vector<vector<Edge>> disjoint_augmenting_paths
+) {
     // Takes a vector (list) of disjoint augmenting paths and adds them to the matching.
 
     for (vector<Edge> augmenting_path : disjoint_augmenting_paths) {
@@ -51,7 +119,11 @@ Matching augment_matching(Matching matching, vector<vector<Edge>> disjoint_augme
     return matching;
 }
 
-Matching algorithm(Stream* stream, Matching matching, float epsilon) {
+Matching algorithm(
+    Stream* stream,
+    Matching matching,
+    float epsilon
+) {
 
     float scale_limit = (epsilon * epsilon) / 64;
 
@@ -67,7 +139,9 @@ Matching algorithm(Stream* stream, Matching matching, float epsilon) {
     return matching;
 }
 
-Matching get_2_approximate_matching(Stream* stream) {
+Matching get_2_approximate_matching(
+    Stream* stream
+) {
     std::set<int> involved_in_matching = set<int>();
     Matching matching;
 
@@ -85,6 +159,7 @@ Matching get_2_approximate_matching(Stream* stream) {
             matching.insert(edge);
         }
 
+        // Reading next edge
         edge = stream->readStream();
     }
 
